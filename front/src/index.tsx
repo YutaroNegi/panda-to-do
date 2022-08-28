a59fb259cb58b3d88f6376b0ac37057f5a382379
Yutaro Negi <yutaro.sousa@liveuniversity.com>
2022-08-28 08:10:55 -0300

    first commit

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+// redux
+import store from './redux/store'
+import { Provider } from 'react-redux'
+
+// css
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
